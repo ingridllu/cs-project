a7558a7fc7c631fe0b42ea58fd2d13bece507f7c
ingrid <ingrid_lu@cate.org>
2022-11-01 13:16:45 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    console.log("hello world")
+  }
+  
+  function draw() {
+    background(0,255,0);
+  }
+
+  movingPlate(){
+    ellipse
+    how do i make it move left and right?
+  }
---
+++ suitcase.js
@@ -0,0 +1,13 @@
+class suitcase{
+    constructor (x,y,color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    suitcase (
+        y=0
+        rect(20, 50, random(0, windowWidth), y)
+        y--
+    )
+
+}
